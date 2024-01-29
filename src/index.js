32580e1b81e82c41db8cb4e4621dd2f23d0901ac
sashaij <o.zagorodniuk@outlook.com>
2024-01-29 19:42:51 +0200

    Add import statement for styles

--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ console.log('something');
 
 import fillContent from "./home-content.js";
 import removeContent from "./remove-content.js";
+import './styles/main.css'
 
 
 console.log(fillContent());
