e2d0118c4d69ea1392394c2e28aa29832331e4b0
sashaij <o.zagorodniuk@outlook.com>
2024-01-31 20:23:36 +0200

    Menu tab in progress

---
+++ src/menu-content.js
@@ -0,0 +1,20 @@
+
+const menuContent = document.createElement('div');
+menuContent.setAttribute('id', 'menu-content');
+
+menuContent.innerHTML = `
+<div class="header">The best restaurant</div>
+<div class="main">
+    <div class="cont-container">
+        <div id="page-header">Our Restaurant</div>
+        <div class="info-field" id="description">
+            Text Text Text Text Text Text Text Text Text Text Text Text Text Text Text Text Text Text Text Text Text Text Text Text Text Text Text Text Text Text Text Text Text Text Text Text Text Text Text Text Text Text Text Text Text Text Text Text Text Text Text Text Text Text Text Text Text Text Text Text Text Text Text Text Text Text Text Text Text Text Text Text Text Text Text Text Text Text Text Text
+        </div>
+        <div class="info-field" id="working-hours">
+            Text Text Text Text Text Text Text Text Text Text Text Text Text Text Text Text Text Text Text Text Text Text Text Text Text Text Text Text Text Text Text Text Text Text Text Text Text Text Text Text Text Text Text Text Text Text Text Text Text Text Text Text Text Text Text Text Text Text Text Text Text Text Text Text Text Text Text Text Text Text Text Text Text Text Text Text Text Text Text Text
+        </div>
+        <div class="info-field" id="address">
+            Text Text Text Text Text Text Text Text Text Text Text Text Text Text Text Text Text Text Text Text Text Text Text Text Text Text Text Text Text Text Text Text Text Text Text Text Text Text Text Text Text Text Text Text Text Text Text Text Text Text Text Text Text Text Text Text Text Text Text Text Text Text Text Text Text Text Text Text Text Text Text Text Text Text Text Text Text Text Text Text
+        </div>
+    </div>
+`
